6f7ef6ce9f15574edab43140a6978cae7851a65a
Bogdoing <bogdoingg@gmail.com>
2024-04-16 23:54:31 +0300

    rebuild app

---
+++ script/build.ts
@@ -0,0 +1,12 @@
+import { bot } from '../src/bot'
+
+// const {
+//     VERCEL_URL: host,
+//     // set your webhook address or use default Vercel deployment url
+//     WEBHOOK: webhook = `https://${host}/api/webhook`,
+// } = process.env
+
+const host    = process.env.VERCEL_URL || ''
+const webhook = process.env.WEBHOOK    || 'https://80/api/webhook'
+
+void bot.api.setWebhook(webhook)
